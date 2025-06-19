595c7ff22a4c20b2a49f56ce3aa4696b6d53f2d5
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-06-19 12:23:16 +0700

    feat: add admin edit location functionality
    - Implemented the admin edit location page with a form to update location details.
    - Created `AdminEditLocationForm` component using `react-hook-form` and `zod` for form handling and validation.
    - Integrated `updateLocation` action to handle the update logic.
    - Added success and error toast notifications to provide feedback to the user.
    - Included a back button to navigate to the admin locations page.

---
+++ src/app/admin/(dashboard)/locations/edit/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { getLocationById } from "@/lib/actions/admin.location.actions";
+import AdminEditLocationForm from "@/components/features/location/admin/admin-edit-location-form";
+
+export const metadata: Metadata = {
+  title: "Admin Edit Location | SE",
+  description: "Admin Edit Location",
+};
+
+export default async function AdminEditLocationPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const data = await getLocationById(id);
+
+  if (!data) {
+    return redirect("/admin/locations");
+  }
+
+  return (
+    <section className="container mx-auto py-10">
+      <h1 className="text-2xl font-bold">Edit Location</h1>
+      <AdminEditLocationForm initialData={data} />
+    </section>
+  );
+}
---
+++ src/components/features/location/admin/admin-edit-location-form.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod/v4";
+import toast from "react-hot-toast";
+import { Location } from "@prisma/client";
+import { adminLocationSchema as formSchema  } from "@/types/admin.location.types";
+import { updateLocation } from "@/lib/actions/admin.location.actions";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+export default function AdminEditLocationForm({ initialData }: { initialData: Location }) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: initialData.name || "",
+    },
+  });
+  const router = useRouter();
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const result = await updateLocation(initialData.id, values);
+
+    if (result.success) {
+      toast.success(result.message);
+      router.push("/admin/locations");
+      form.reset();
+    } else {
+      toast.error(result.message);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form method="POST" onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 py-6">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Location Name" {...field} required autoFocus />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex items-center justify-end space-x-2">
+          <Button size="sm" variant="outline" type="button" asChild>
+            <Link href="/admin/locations">Back</Link>
+          </Button>
+          <Button size="sm" type="submit" disabled={form.formState.isSubmitting}>
+            {form.formState.isSubmitting ? "Editing..." : "Edit Location"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
